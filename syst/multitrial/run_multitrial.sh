9bcd73714bfba43975baffa46495302f4997f540
spolitan <stefano.politano@cern.ch>
2025-12-04 10:01:01 +0100

    Adjusting path in run_multitrial

--- syst/multitrial/run_multitrial.sh
+++ syst/multitrial/run_multitrial.sh
@@ -5,7 +5,7 @@ generate_cutset() {
     trial_num=$(basename "$yaml_file" | sed -E 's/config_trial_([0-9]+)\.yml/\1/')
     target_dir="$dir/trial_$trial_num"
     echo "[$trial_num] Generating YAML cutsets for file $yaml_file into $target_dir"
-    python3 /home/mdicosta/alice/hf-vn/src/make_cutsets_cfgs.py "$yaml_file" -o "$target_dir"
+    python3 $path_to_src/src/make_cutsets_cfgs.py "$yaml_file" -o "$target_dir"
 }
 
 get_v2_vs_fracs() {
@@ -14,7 +14,7 @@ get_v2_vs_fracs() {
     trial_num=$(basename "$yaml_file" | sed -E 's/config_trial_([0-9]+)\.yml/\1/')
     target_dir="$dir/trial_$trial_num"
     echo "[$trial_num] Computing v2 vs frac for file $yaml_file"
-    python3 /home/mdicosta/alice/hf-vn/src/get_v2_vs_frac.py "$yaml_file" "$target_dir/raw_yields/" "$reference_frac_dir" --multitrial
+    python3 $path_to_src/src/get_v2_vs_frac.py "$yaml_file" "$target_dir/raw_yields/" "$reference_frac_dir" --multitrial
 }
 
 # Export function and necessary variables
@@ -22,34 +22,35 @@ export -f generate_cutset
 export -f get_v2_vs_fracs
 export dir  # make sure $dir is visible inside the function
 
-export n_parallel=20
-export config_modifies="/home/mdicosta/FlowDplus/TestRCT/systMultitrial.yml"
-export config_default="/home/mdicosta/FlowDplus/TestRCT/config.yml"
-export output_dir="/home/mdicosta/FlowDplus/TestRCT/cutvar_RCTFlagsOn_combined"
-export do_cutset_generation=false
-export do_compile_fitter=false
-export do_projections=false
-export do_fitting=false
+export n_parallel=10
+export path_to_src="/home/spolitan/alice/hf-vn/"
+export config_modifies="/home/spolitan/alice/hf-vn/syst/multitrial/systMultitrial.yml"
+export config_default="/home/spolitan/alice/hf-vn/configs/test_config.yml"
+export output_dir="/home/spolitan/alice/ds_v2_oo/output/031225"
+export do_cutset_generation=true
+export do_compile_fitter=true
+export do_projections=true
+export do_fitting=true
 export do_v2_vs_frac=false
-export produce_plots=true
+export produce_plots=false
 
 mkdir -p "$output_dir/syst/multitrial"
 
 
 if [ "$do_cutset_generation" = true ]; then
     # Generate YAML file list with indices
-    echo "Generating yaml files for systematics ..."
-    python3 /home/mdicosta/alice/hf-vn/syst/multitrial/make_cutsets_multitrial.py $config_default -m $config_modifies -o $output_dir > "$output_dir/syst/multitrial/log_make_cutsets.txt" 2>&1
+    python3 $path_to_src/syst/multitrial/make_cutsets_multitrial.py $config_default -m $config_modifies -o $output_dir > "$output_dir/syst/multitrial/log_make_cutsets.txt" 2>&1
     echo "Yaml files generated!"
 fi
 
 if [ "$do_compile_fitter" = true ]; then
     echo "Compiling InvMassFitter and VnVsMassFitter ..."
-    rootcling -f ./invmassfitter/vnfitter_dict.cxx -c ./invmassfitter/InvMassFitter.h ./invmassfitter/VnVsMassFitter.h ./syst/multitrial/LinkDefVnFitter.h
+    rootcling -f $path_to_src/invmassfitter/vnfitter_dict.cxx -c /home/spolitan/alice/hf-vn/invmassfitter/InvMassFitter.h /home/spolitan/alice/hf-vn/invmassfitter/VnVsMassFitter.h /home/spolitan/alice/hf-vn/syst/multitrial/LinkDefVnFitter.h
 
+    echo "Compiling shared library ..."
     g++ -shared -fPIC `root-config --cflags --libs` \
-        ./invmassfitter/InvMassFitter.cxx ./invmassfitter/VnVsMassFitter.cxx ./invmassfitter/vnfitter_dict.cxx \
-        -o ./invmassfitter/libvnfitter.so
+        $path_to_src/invmassfitter/InvMassFitter.cxx $path_to_src/invmassfitter/VnVsMassFitter.cxx $path_to_src/invmassfitter/vnfitter_dict.cxx \
+        -o $path_to_src/invmassfitter/libvnfitter.so
     echo "Compilation done!"
 fi
 
@@ -76,7 +77,8 @@ for dir in "${pt_dirs[@]}"; do
     # --- Perform projections ---
     if [ "$do_projections" = true ]; then
         start=$(date +%s)
-        python3 /home/mdicosta/alice/hf-vn/src/proj_thn.py $config_default --multitrial_folder "$dir" > "$log_file" 2>&1
+        echo "python3 $path_to_src/src/proj_thn.py $config_default --multitrial_folder "$dir" > "$log_file" 2>&1"
+        python3 $path_to_src/src/proj_thn.py $config_default --multitrial_folder "$dir" > "$log_file" 2>&1
         end=$(date +%s)
         echo -e "----> Projections performed in $((end - start)) seconds. Starting fitting ..."
     fi
@@ -84,7 +86,7 @@ for dir in "${pt_dirs[@]}"; do
     # --- Perform fitting ---
     if [ "$do_fitting" = true ]; then
         start=$(date +%s)
-        python3 /home/mdicosta/alice/hf-vn/syst/multitrial/run_fits.py "${yaml_files[@]}" --nproc $n_parallel > "$log_file" 2>&1
+        python3 $path_to_src/syst/multitrial/run_fits.py "${yaml_files[@]}" --nproc $n_parallel > "$log_file" 2>&1
         end=$(date +%s)
         echo -e "----> Fitting done in $((end - start)) seconds. Starting v2 vs frac extraction ..."
     fi
@@ -108,6 +110,6 @@ done
 # --- Produce final plots ---
 if [ "$produce_plots" = true ]; then
     echo "Producing final systematic plots ..."
-    python3 /home/mdicosta/alice/hf-vn/syst/multitrial/produce_multitrial_syst_plots.py "$output_dir" > "$output_dir/syst/multitrial/log_produce_plots.txt" 2>&1
+    python3 $path_to_src/syst/multitrial/produce_multitrial_syst_plots.py "$output_dir" > "$output_dir/syst/multitrial/log_produce_plots.txt" 2>&1
     echo "Final plots produced!"
 fi
