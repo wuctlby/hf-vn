fb2b84fe7813cbd69b7b92b6d46bd51214795717
spolitan <stefano.politano@cern.ch>
2026-01-12 15:05:59 +0100

    Minor fix in fFD systematics

--- syst/frac/produce_frac_syst_plots.py
+++ syst/frac/produce_frac_syst_plots.py
@@ -47,6 +47,7 @@ def compute_syst(infiles, outputdir, suffix):
     hdeltav2_prompt = []
     hdeltav2_fd = []
     gv2vsfrac = {}
+    tfiles = []
     for ifile, file in enumerate(infiles):
         logger(f'Processing file {file}')
         tfile = TFile().Open(file)
@@ -54,47 +55,69 @@ def compute_syst(infiles, outputdir, suffix):
         if not tfile.GetListOfKeys().Contains('hV2VsPtFD') or not tfile.GetListOfKeys().Contains('hV2VsPtPrompt'):
             logger(f'File {file} does not contain the required histograms, skipping it', level='WARNING')
             continue
-        hv2_prompt.append(tfile.Get('hV2VsPtPrompt'))
-        hv2_fd.append(tfile.Get('hV2VsPtFD'))
+        tfiles.append(tfile)
+        hv2prompt = tfiles[-1].Get('hV2VsPtPrompt')
+        hv2fd = tfiles[-1].Get('hV2VsPtFD')
+        if hv2prompt is None or hv2fd is None:
+            hv2prompt.SetDirectory(0)
+            hv2fd.SetDirectory(0)
+        hv2_prompt.append(hv2prompt)
+        hv2_fd.append(hv2fd)
 
         gv2vsfrac[labels[ifile]] = []
         for ibin in range(1, hv2_prompt[0].GetNbinsX()+1):
             ptmin = hv2_prompt[0].GetXaxis().GetBinLowEdge(ibin)
             ptmax = hv2_prompt[0].GetXaxis().GetBinLowEdge(ibin) + hv2_prompt[0].GetXaxis().GetBinWidth(ibin)
-            gv2vsfrac[labels[ifile]].append(tfile.Get(f'pt_{ptmin*10:.0f}_{ptmax*10:.0f}/gV2VsFrac'))
-            SetObjectStyle(gv2vsfrac[labels[ifile]][-1], color=cols[ifile], fillcolor=cols[ifile], markerstyle=kFullCircle, markersize=2, linewidth=2, fillalpha=0.2)
-
-        hv2_prompt[-1].SetDirectory(0)
-        hv2_fd[-1].SetDirectory(0)
+            gv2_vs_frc = tfiles[-1].Get(f'pt_{ptmin*10:.0f}_{ptmax*10:.0f}/gV2VsFrac').Clone(f'gv2vsfrac_pt{ptmin:.1f}_{ptmax:.1f}_{labels[ifile]}')
+            gv2_vs_frc.SetTitle(f'{(ptmin*10):.0f} < #it{{p}}_{{T}} < {(ptmax*10):.0f} GeV/#it{{c}}')
+            gv2_vs_frc.GetXaxis().SetTitle('#it{f}_{FD}')
+            gv2_vs_frc.GetYaxis().SetTitle(f'#it{{v}}_{{2}} ({ptmin:.1f} < #it{{p}}_{{T}} < {ptmax:.1f} GeV/#it{{c}})')
+            gv2_vs_frc.GetYaxis().SetRangeUser(-0.1, 0.3)
+            gv2_vs_frc.GetYaxis().SetDecimals()
+            gv2_vs_frc.GetXaxis().SetRangeUser(0, 1)
+            gv2vsfrac[labels[ifile]].append(gv2_vs_frc)
+            SetObjectStyle(gv2vsfrac[labels[ifile]][-1], color=cols[ifile], fillcolor=cols[ifile], markerstyle=kFullCircle, markersize=1.4, linewidth=3, fillalpha=0.2)
         SetObjectStyle(hv2_prompt[-1], color=cols[ifile], fillcolor=cols[ifile], markerstyle=kFullCircle, markersize=1.4, linewidth=3)
         SetObjectStyle(hv2_fd[-1], color=cols[ifile], fillcolor=cols[ifile], markerstyle=kFullSquare, markersize=1.4, linewidth=3)
 
+        if ifile == 0:
+            continue  # skip the default for the delta v2
         hdeltav2_prompt.append(get_hdeltav(hv2_prompt[0], hv2_prompt[ifile]))
         hdeltav2_fd.append(get_hdeltav(hv2_fd[0], hv2_fd[ifile]))
     gsyst_prompt = get_rms_shift(hdeltav2_prompt)
     gsyst_fd = get_rms_shift(hdeltav2_fd)
 
-    canv_fFD = TCanvas('canv', 'canv', 1600, 1600)
-    canv_fFD.Divide(4, 4)
-    for igs, gfracs in enumerate(gv2vsfrac):
-        for ig, gfrac in enumerate(gv2vsfrac[gfracs]):
-            canv_fFD.cd(ig+1)
-            if igs == 0:
-                gfrac.Draw('pea')
-            else:
-                gfrac.Draw('same pe')
-    canv_fFD.Update()
+    canv_fFD = TCanvas('canv', 'canv', 1400, 800)
+    if len(gv2vsfrac[labels[0]]) <= 6:
+        canv_fFD.Divide(3, 2)
+    else:
+        canv_fFD.Divide(5, 2)
 
-    #______________________________________________________________________________________
-    # Plot the systematics
-    canv = TCanvas('canv', 'canv', 1600, 1600)
-    canv.Divide(2, 2)
-    
     legv2 = TLegend(0.6, 0.6, 0.9, 0.9)
     legv2.SetBorderSize(0)
     legv2.SetFillStyle(0)
     legv2.SetTextSize(0.03)
     legv2.SetTextFont(42)
+    for igs, gfracs in enumerate(gv2vsfrac):
+        for ig, gfrac in enumerate(gv2vsfrac[gfracs]):
+            canv_fFD.cd(ig+1)
+            canv_fFD.cd(ig+1).SetBottomMargin(0.16)
+            canv_fFD.cd(ig+1).SetLeftMargin(0.16)
+            if igs == 0:
+                gfrac.Draw('peaz')
+                if ig == len(gv2vsfrac[gfracs]) - 1:
+                    legv2.Draw()
+            else:
+                gfrac.Draw('same pez')
+            if ig == 0:
+                legv2.AddEntry(hv2_prompt[igs], labels[igs], 'ep')
+    canv_fFD.Update()
+    canv_fFD.SaveAs(f'{outputdir}/gV2vsFrac_{suffix}.png')
+
+    #______________________________________________________________________________________
+    # Plot the systematics
+    canv = TCanvas('canv', 'canv', 1600, 1600)
+    canv.Divide(2, 2)
     
     legsyst = TLegend(0.6, 0.79, 0.9, 0.89)
     legsyst.SetBorderSize(0)
@@ -110,7 +133,6 @@ def compute_syst(infiles, outputdir, suffix):
         hist.GetYaxis().SetTitle('prompt #it{v}_{2}')
         hist.GetXaxis().SetRangeUser(0, 25)
         hist.GetYaxis().SetRangeUser(-0.1, 0.35)
-        legv2.AddEntry(hist, labels[ihist], 'ep')
         hist.Draw('same')
     legv2.Draw()
     canv.cd(2).SetLeftMargin(0.16)
@@ -128,7 +150,7 @@ def compute_syst(infiles, outputdir, suffix):
         hist.GetXaxis().SetTitle('#it{p}_{T} (GeV/#it{c})')
         hist.GetYaxis().SetTitle('prompt #it{v}_{2}^{syst.} #minus #it{v}_{2}^{ref.}')
         hist.GetXaxis().SetRangeUser(0, 25)
-        hist.GetYaxis().SetRangeUser(-0.05, 0.05)
+        hist.GetYaxis().SetRangeUser(-0.001, 0.001)
         hist.Draw('samepe')
     gsyst_prompt.Draw('same5')
     legsyst.AddEntry(gsyst_prompt, f'#sqrt{{shift^{{2}} + rms^{{2}}}}', 'f')
@@ -140,45 +162,44 @@ def compute_syst(infiles, outputdir, suffix):
         hist.GetXaxis().SetTitle('#it{p}_{T} (GeV/#it{c})')
         hist.GetYaxis().SetTitle('non-prompt #it{v}_{2}^{syst.} #minus #it{v}_{2}^{ref.}')
         hist.GetXaxis().SetRangeUser(0, 25)
-        hist.GetYaxis().SetRangeUser(-0.05, 0.05)
+        hist.GetYaxis().SetRangeUser(-0.006, 0.004)
         hist.Draw('samepe')
     gsyst_fd.Draw('same5')
 
     canv.Update()
 
-    canvSyst = TCanvas('canvSyst', 'canvSyst', 1200, 600)
-    canvSyst.Divide(2, 1)
-    canvSyst.cd(1).SetLeftMargin(0.16)
-    canvSyst.cd(1).SetLogy()
-    canvSyst.cd(1).SetGridy()
+    canvSystPrompt = TCanvas('canvSystPrompt', 'canvSystPrompt', 800, 800)
+    canvSystPrompt.SetLeftMargin(0.16)
+    canvSystPrompt.SetLogy()
+    canvSystPrompt.SetGridy()
     gsyst_prompt.SetStats(0)
     gsyst_prompt.GetXaxis().SetTitle('#it{p}_{T} (GeV/#it{c})')
     gsyst_prompt.GetYaxis().SetTitle('Systematic uncertainty on prompt #it{v}_{2}')
     gsyst_prompt.GetXaxis().SetRangeUser(0, 25)
     gsyst_prompt.GetYaxis().SetRangeUser(1.e-5, 0.05)
     gsyst_prompt.Draw('a5')
-    canvSyst.cd(2).SetLeftMargin(0.16)
-    canvSyst.cd(2).SetLogy()
-    canvSyst.cd(2).SetGridy()
+    canvSystPrompt.Update()
+
+    canvSystFD = TCanvas('canvSystFD', 'canvSystFD', 800, 800)
+    canvSystFD.SetLeftMargin(0.16)
+    canvSystFD.SetLogy()
+    canvSystFD.SetGridy()
     gsyst_fd.SetStats(0)
     gsyst_fd.GetXaxis().SetTitle('#it{p}_{T} (GeV/#it{c})')
     gsyst_fd.GetYaxis().SetTitle('Systematic uncertainty on non-prompt #it{v}_{2}')
     gsyst_fd.GetXaxis().SetRangeUser(0, 25)
     gsyst_fd.GetYaxis().SetRangeUser(1.e-5, 0.05)
     gsyst_fd.Draw('a5')
-    canvSyst.Update()
+    canvSystFD.Update()
 
     #______________________________________________________________________________________
     # Save output
     outputfile = os.path.join(outputdir, f'syst_fFD_{suffix}.root')
     logger(f'Saving output to {outputfile}')
-    canv.SaveAs(f'{outputdir}/SystfFD_{suffix}.root') 
-    canv.SaveAs(f'{outputdir}/SystfFD_{suffix}.pdf')
-    canv.SaveAs(f'{outputdir}/SystfFD_{suffix}.png')
-
-    canvSyst.SaveAs(f'{outputdir}/SystOnlyfFD_{suffix}.root') 
-    canvSyst.SaveAs(f'{outputdir}/SystOnlyfFD_{suffix}.pdf')
-    canvSyst.SaveAs(f'{outputdir}/SystOnlyfFD_{suffix}.png')
+    for outformat in ['root', 'pdf', 'png']:
+        canv.SaveAs(f'{outputdir}/SystfFD_{suffix}.{outformat}')
+        canvSystPrompt.SaveAs(f'{outputdir}/SystOnlyfPrompt_{suffix}.{outformat}')
+        canvSystFD.SaveAs(f'{outputdir}/SystOnlyfFD_{suffix}.{outformat}')
 
 
 if __name__ == "__main__":
